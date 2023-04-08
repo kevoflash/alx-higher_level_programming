47aa2bb1326acda923a470e28f5e69afc760c63e
kevoflash <kelvinmugao@gmail.com>
2023-04-08 07:53:37 -0700

    Longest task

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,12 +1,8 @@
+#!/usr/bin/python3
 class LockedClass:
     """
     A class that prevents the user from dynamically creating new instance attributes,
     except for the attribute called first_name.
     """
     __slots__ = ['first_name']
-
-    def __setattr__(self, name, value):
-        if name != 'first_name':
-            raise AttributeError(f"{self.__class__.__name__} does not support attribute assignment")
-        self.__dict__[name] = value
 
