5998da7966cd7199d1a4b8f308095935c1cb150a
kevoflash <kelvinmugao@gmail.com>
2023-09-20 02:42:14 -0700

    fix

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -10,25 +10,44 @@
  */
 void print_python_string(PyObject *p)
 {
+    PyObject *encoded_str;
+    PyObject *utf8_str;
 
-	PyObject *str, *repr;
+    printf("[.] string object info\n");
 
-	(void)repr;
-	printf("[.] string object info\n");
+    if (PyUnicode_Check(p))
+    {
+        if (PyUnicode_IS_COMPACT_ASCII(p))
+            printf("  type: compact ascii\n");
+        else
+            printf("  type: compact unicode object\n");
 
-	if (strcmp(p->ob_type->tp_name, "str"))
-	{
-		printf("  [ERROR] Invalid String Object\n");
-		return;
-	}
+        printf("  length: %ld\n", PyUnicode_GET_LENGTH(p));
 
-	if (PyUnicode_IS_COMPACT_ASCII(p))
-		printf("  type: compact ascii\n");
-	else
-		printf("  type: compact unicode object\n");
-
-	repr = PyObject_Repr(p);
-	str = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
-	printf("  length: %ld\n", PyUnicode_GET_SIZE(p));
-	printf("  value: %s\n", PyBytes_AsString(str));
+        // Convert the Python Unicode object to a UTF-8 encoded string
+        encoded_str = PyUnicode_AsUTF8String(p);
+        if (encoded_str != NULL)
+        {
+            // Convert the encoded string to a C string
+            utf8_str = PyBytes_AsString(encoded_str);
+            if (utf8_str != NULL)
+            {
+                printf("  value: %s\n", utf8_str);
+            }
+            else
+            {
+                printf("  [ERROR] Failed to convert to UTF-8 string\n");
+            }
+            // Don't forget to release the memory
+            Py_XDECREF(encoded_str);
+        }
+        else
+        {
+            printf("  [ERROR] Failed to encode string to UTF-8\n");
+        }
+    }
+    else
+    {
+        printf("  [ERROR] Invalid String Object\n");
+    }
 }
